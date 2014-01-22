8ae45c7b743009417615b69470f6a763db08cc31
Miska Noponen <miska.noponen@gmail.com>
2014-01-22 11:12:51 +0200

    luokkakaavion eka versio

--- henkilostokysely/src/main/java/javalabra/henkilostokysely/App.java
+++ henkilostokysely/src/main/java/javalabra/henkilostokysely/App.java
@@ -1,13 +1,10 @@
 package javalabra.henkilostokysely;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       // tähän koodia
     }
 }
